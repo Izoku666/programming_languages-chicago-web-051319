4fc5fc92f249fdfea56e87cc25d5029bc9a11c4b
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-24 01:42:36 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -15,6 +15,8 @@ def reformat_languages(languages)
         #puts "Has no style, adding it"
         a[langName][:style] = []
         a[langName][:style] << style
+      else
+        puts "THIS IS JAVASCRIPT"
       end
       
       if langName == :javascript
