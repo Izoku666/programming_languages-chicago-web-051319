71cb10bf7df1739ac908bfbfc36c9ae3a90d74c3
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-24 01:55:02 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -12,7 +12,7 @@ def reformat_languages(languages)
       
             
       if a[langName].include?(:style) == false
-        puts "Has no style, adding it"
+        puts "#{langName} Has no style, adding it"
         a[langName][:style] = []
         puts a[langName][:style]
         #a[langName][:style] << style
