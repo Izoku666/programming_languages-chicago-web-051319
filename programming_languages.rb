6e4da32d0e50a0ced575f3762891ecd21d6bdddd
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-24 01:08:44 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
   # your code here
+  a = {}
+  languages.each do |style , langs|
+    langs.each do |langName , type|
+      a[langName] = type
+    end
+  end
+  
+  puts a
 end
